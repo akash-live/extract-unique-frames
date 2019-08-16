fd0172cae856aa2f27248e0d3517d13160a3b36f
akash-live <54156077+akash-live@users.noreply.github.com>
2019-08-16 18:50:44 -0400

    Update extract_frames.py

--- extract_frames.py
+++ extract_frames.py
@@ -1,64 +1,64 @@
-# Program To Read video and Extract Unique Frames
+# To Read video and Extract Unique Frames
+# Paste the file in the directory with videos
+
 import cv2
 import os
-from skimage.measure import compare_mse, compare_nrmse, compare_ssim, compare_psnr
+from skimage.measure import compare_mse
 
 
 # Function to extract frames
-def framecapture(path):
+def frame_capture(path):
 
     # Get file list sorted by date modified
-    filelist = [s for s in os.listdir(path)
+    file_list = [s for s in os.listdir(path)
          if os.path.isfile(os.path.join(path, s))]
-    filelist.sort(key=lambda s: os.path.getmtime(os.path.join(path, s)))
+    file_list.sort(key=lambda s: os.path.getmtime(os.path.join(path, s)))
 
-    for file in filelist:
+    for file in file_list:
         # Path to video file
-        vidObj = cv2.VideoCapture(os.path.join(path, file))
+        vid_obj = cv2.VideoCapture(os.path.join(path, file))
         print(file)
         # Frame counter
         count = 0
 
         # Get first frame
         # success = 1
-        success, image = vidObj.read()
+        success, image = vid_obj.read()
         try:
             os.mkdir(path + "\\Frames")
         except FileExistsError:
             pass
         while success:
             # function extract frames
-            success, nextimage = vidObj.read()
-
+            success, next_image = vid_obj.read()
             try:
                 # Comparing frames
-                if compare_mse(nextimage, image) > 3:
-                    print("Diffrence Factor : " + str(compare_mse(nextimage, image)))
+                if compare_mse(next_image, image) > 3:
+                    print("Difference Factor : " + str(compare_mse(next_image, image)))
                     # Saves the frames with frame-count
-                    cv2.imwrite(path + '\\Frames\\' + file + "_frame%d.jpg" % count, nextimage)
-
-                    # resize image
+                    cv2.imwrite(path + '\\Frames\\' + file + "_frame%d.jpg" % count, next_image)
+                    # # resize image
                     # scale_percent = 20  # percent of original size
-                    # width = int(nextimage.shape[1] * scale_percent / 100)
-                    # height = int(nextimage.shape[0] * scale_percent / 100)
+                    # width = int(next_image.shape[1] * scale_percent / 100)
+                    # height = int(next_image.shape[0] * scale_percent / 100)
                     # dim = (width, height)
-                    # cv2.imshow('image', cv2.resize(nextimage, dim, interpolation=cv2.INTER_AREA))
+                    # cv2.imshow('image', cv2.resize(next_image, dim, interpolation=cv2.INTER_AREA))
                     # cv2.waitKey(1000)
-
                     print("Path : " + path + '\\Frames\\' + file + "_frame%d.jpg" % count)
                     print("--------------------------------------------------------")
                     # else:
-                    image = nextimage
+                    image = next_image
                 # Advance 60 frames = 1 sec
                 count += 60
-                vidObj.set(1, count)
+                vid_obj.set(1, count)
             except AttributeError:
                 print('Next')
                 print("--------------------------------------------------------")
     cv2.destroyAllWindows()
 
+
 # Driver Code
 if __name__ == '__main__':
-    # Calling the function
 
-    framecapture(input("Enter path : "))
+    # Calling the function
+    frame_capture(os.getcwd())
